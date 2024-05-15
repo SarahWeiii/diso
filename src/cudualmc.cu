28f7090c2b330ccb2916cbed1eb12803dc08d14f
Xinyue Wei <13659185706@163.com>
2024-05-15 13:12:59 -0700

    fix mask condition

--- src/cudualmc.cu
+++ src/cudualmc.cu
@@ -640,7 +640,7 @@ namespace cudualmc
     // if (valid != 8)
     //   printf("code %d valid %d\n", code, valid);
 
-    if (code != 0 && code != 255 && valid > 4)
+    if (code != 0 && code != 255 && valid == 8)
     {
       dmc.first_cell_used[cell_index] = 1;
     }
